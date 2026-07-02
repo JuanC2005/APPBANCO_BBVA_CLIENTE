cc1f66e4e98290440e71a933e400b84de77632ad
Juan Castro <juancastroverastegui@gmail.com>
2026-07-02 09:09:51 -0500

    arreglo consumo de api

--- lib/core/api_config.dart
+++ lib/core/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  static const String baseUrl = 'http://10.0.2.2:8000/api/v1';
+  static const String baseUrl = 'https://appbanco-bbva-api.onrender.com/api/v1';
   static const Duration timeout = Duration(seconds: 30);
 }
